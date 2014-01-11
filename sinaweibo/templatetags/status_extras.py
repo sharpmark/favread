0e51b37ff15450789d2e36a61982a5feda6118af
sharpmark <sharpmark@gmail.com>
2014-01-11 12:49:32 +0800

    网页显示优化
    0. 增加了应用logo
    1. fix user，status url
    2. 优化了几个链接样式
    3. 增加了网页标题
    4. 图片显示优化为使用square图片，以保持统一
    5. @正则匹配优化，支持“）：”的正确分隔

--- sinaweibo/templatetags/status_extras.py
+++ sinaweibo/templatetags/status_extras.py
@@ -33,7 +33,7 @@ def add_topic(v):
 @register.filter
 def statusfmt(value):
     #pattern_at = re.compile(r'\@[^\s\&\:\)\uff09\uff1a\@]+')
-    pattern_at = re.compile(r'\@[^\s\&\:\)\@]+')
+    pattern_at = re.compile(u'\@[^\s\&\:\)\@\uff1a\uff09]+')
     pattern_topic = re.compile(r'\#[^\#]+\#')
     pattern_link = re.compile(r'http\:\/\/[a-zA-Z0-9\_\/\.\-]+')
 
@@ -43,3 +43,86 @@ def statusfmt(value):
     value = pattern_at.sub(add_at, value)
     value = pattern_topic.sub(add_topic, value)
     return value
+
+@register.filter
+def imagefmt(value):
+    return value.replace('thumbnail', 'square')
+
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ 
+def base62_encode(num, alphabet=ALPHABET):
+    """Encode a number in Base X
+ 
+    `num`: The number to encode
+    `alphabet`: The alphabet to use for encoding
+    """
+    if (num == 0):
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        rem = num % base
+        num = num // base
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+ 
+def base62_decode(string, alphabet=ALPHABET):
+    """Decode a Base X encoded string into the number
+ 
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for encoding
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+ 
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+ 
+    return num
+
+def url2mid(url):
+    '''
+    >>> url_to_mid('z0JH2lOMb')
+    3501756485200075L
+    >>> url_to_mid('z0Ijpwgk7')
+    3501703397689247L
+    '''
+    url = str(url)[::-1]
+    size = len(url) / 4 if len(url) % 4 == 0 else len(url) / 4 + 1
+    result = []
+    for i in range(size):
+        s = url[i * 4: (i + 1) * 4][::-1]
+        s = str(base62_decode(str(s)))
+        s_len = len(s)
+        if i < size - 1 and s_len < 7:
+            s = (7 - s_len) * '0' + s
+        result.append(s)
+    result.reverse()
+    return int(''.join(result))
+
+@register.filter
+def mid2url(value):
+    '''
+    >>> mid_to_url(3501756485200075)
+    'z0JH2lOMb'
+    >>> mid_to_url(3501703397689247)
+    'z0Ijpwgk7'
+    '''
+    value = str(value)[::-1]
+    size = len(value) / 7 if len(value) % 7 == 0 else len(value) / 7 + 1
+    result = []
+    for i in range(size):
+        s = value[i * 7: (i + 1) * 7][::-1]
+        s = base62_encode(int(s))
+        s_len = len(s)
+        if i < size - 1 and len(s) < 4:
+            s = '0' * (4 - s_len) + s
+        result.append(s)
+    result.reverse()
+    return ''.join(result)
